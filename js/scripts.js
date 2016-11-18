1e4da1b12966106dc04c64b9efdc1128b85cfdeb
Felipe Peláez Covatti <felipe_p.c@hotmail.com>
2016-11-18 15:01:21 -0200

    style amost ready. only support images (not video yet)

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+
+
+ 	// prevent default behavor in anchors of gallery images
+$("a[href^='img/'],a[href^='https://www.youtube.com']").click(function(event){
+    event.preventDefault();
+});
+
+
+
+	// get the link of the clicked image
+
+
+$("a[href^='img/']").click(function() {
+   	var clickedImg = $(this).attr('href');
+    $( '.lightbox-ctnr' ).css("display", "flex");
+    $(".lightbox-ctnr img").attr("src", clickedImg);
+ });
+
+ $(".lightbox-ctnr").click(function() {
+ 	$(".lightbox-ctnr").css("display", "none");
+ });
+
+
+
+
+	// When someone click on the image
+	// get the identifier of the image (ex 01, 02)
+
+		// show a new box with the proper image placed
+
+			// obs: this box has to be styles according to the image provided
+			// the rest of the page have to be defoused
+
+		// show previus and next button
+
+			// the previos and next button have to work, being able 
+			// to add and subtract to the img identifier
+			// it also has to disable previous in the fist image and next in the last image
+
