1d00d0bae8f83416ea1a494186cb6cd9048a05e8
Felipe Peláez Covatti <felipe_p.c@hotmail.com>
2016-11-23 01:35:37 -0200

    style working and tested in ie, firefox, chrome and edge

--- js/scripts.js
+++ js/scripts.js
@@ -1,103 +1,84 @@
-// prevent default behavor in anchors of gallery images and videos
-$(".gallery-grid a").click(function(event) {
-    event.preventDefault();
-});
+// -----------Global variables------------- //
 
-// Get the size of the browser scrollbar
+        var imgW;
+        var imgH;
+        var imgRatio;
+        var legH;
+
+// ----------------end------------------- //
+
+// prevent default behavor in anchors of gallery images and videos //
+
+    $(".gallery-grid a").click(function(event) {
+        event.preventDefault();
+    });
+
+// ----------------end------------------- //
+
+// Get the size of the browser scrollbar //
+
 	$("body").css("overflow-y", "hidden");
 	var bodySizeNoScroll = $('body').outerWidth(true);
 	$("body").css("overflow-y", "scroll");
 	var bodySizeInScroll = $('body').outerWidth(true);
 	var scrollSize = bodySizeNoScroll - bodySizeInScroll;
-// end
-            
 
-    // ---------------TESTE---------------
+// ----------------end------------------- //
 
-//             function getImgRatio() {
-
-//                 var imgWt = $(".img-place").outerWidth(true);
-//                 var imgHt = $(".img-place").outerHeight(true);
-//                 var imgRatiot = imgWt / imgHt;
-//                 alert(imgRatiot);
-                           
-//             }
-        
-//         $(".gallery-grid a").click(function makeImgPlace() {
-
-//             var imgHref = $(this).attr('href');
-
-//             $("body").append("<img class='img-place'>");
-               
-//             $(".img-place").attr("src", imgHref);
-         
-
-//             $(".img-place").on("load", function() {
-
-
-//                 getImgRatio();
-    
-//                 $(".img-place").remove();
-         
-
-//             }); 
-         
-       
-
-// });
-
-
-
-
-
-// ----------------------------------------
 
     function responsiveImg() {
 
+            $(".lbox-box img").addClass("reset-img-js");
 
-        $(".lightbox-ctnr img").addClass("reset-img-js");
-        var imgW = $(".lightbox-ctnr img").outerWidth(true);
-        var imgH = $(".lightbox-ctnr img").outerHeight(true);
-        var imgRatio = imgW / imgH;
-        var winH = $(window).height();
-        var figcapH = $(".lightbox-ctnr figcaption").outerHeight(true);
-        $(".lightbox-ctnr img").css("max-height", winH - figcapH + "px");
-        $(".lightbox-ctnr img").css("max-width", imgRatio * (winH - figcapH) + "px");
-        $(".lightbox-ctnr img").removeClass("reset-img-js");
+            imgW = $(".lbox-box img").width();
+            imgH = $(".lbox-box img").height();
+            imgRatio = imgW / imgH;
+            legH = $(".lbox-box figcaption").outerHeight(true);
+
+            $(".lbox-box img").removeClass("reset-img-js");
+
+            newImgW = $(".lbox-box img").width();
+            newImgH = newImgW / imgRatio;
+            $(".lbox-ctnr").css("height", newImgH + legH);
+            $(".lbox-box img").css("height",  newImgH);
+
+    }
 
 
- }
 
 
 
-
-
-$(".gallery-grid a").click(function makeLightBox() {
-
+    $(".gallery-grid a").click(function makeLightBox() {
 
 
     // Remove scrollbar of page in background
     $("body").css("overflow-y", "hidden");
-    // Show lightbox-ctnr
-    $( '.lightbox-ctnr' ).css("display", "flex");
+    // Show lightbox
+    $( '.lbox-shadow' ).css("display", "block");
     // Preserve aspect of the page (remove space gained with the removal of the scrollbar)
     $("body").css("width", "calc(100% - " + scrollSize + "px)");
+
 
     if ($(this).is("a[href^='img/']")) {
 
        		// IMAGE
 
-            // get the link of the clicked image
-            var clickedImg = $(this).attr('href');
+        // get the link of the clicked image
+        var clickedImg = $(this).attr('href');
 
-        $(".lightbox-ctnr .lightbox-box").append("<figure><img><figcaption><p>TESTES TES TSETSa asd as as a asasdfa dsfasdf as asdf asdf asdfa sdfasd TSETES</p></figcaption></figure>");           
-    	$(".lightbox-ctnr img").attr("src", clickedImg);
-        $(".lightbox-ctnr img").on("load", function() {
+        $(".lbox-box").append("<figure><img><figcaption><h2>TESTING</h2><p>TESTES TES TSETSa asd as as a asasdfa dsfasdf as asdf asdf asdfa sdfasd TSETES</p></figcaption></figure>");           
+    	$(".lbox-box img").attr("src", clickedImg);
 
-            responsiveImg();
+                responsiveImg();
+
+        $(".lbox-box img").on("load", function() {
+
+            
+                responsiveImg();
+
+            
     }); 
                 
-      
 
     } else { 
 
@@ -105,10 +86,8 @@ $(".gallery-grid a").click(function makeLightBox() {
 
 			// get the link of the clicked video
     	    var clickedVideo = $(this).attr('href');
-
     		
-
-    	$(".lightbox-ctnr iframe").attr("src", clickedVideo);
+    	// $(".lightbox-ctnr iframe").attr("src", clickedVideo);
 
 
 	}
@@ -116,19 +95,20 @@ $(".gallery-grid a").click(function makeLightBox() {
         
 });
 
-
             $(window).resize( function () {
 
-                 responsiveImg();
+                
+
+                responsiveImg();
+
    
             });
 
 
-
 // Close the lightbox when clicked over
- $(".lightbox-ctnr").click(function() {
-    $(".lightbox-ctnr figure").remove();
- 	$(".lightbox-ctnr").css("display", "none");
+    $(".lbox-shadow").click(function() {
+    $(".lbox-box figure").remove();
+ 	$(".lbox-shadow").css("display", "none");
  	$("body").css("overflow-y", "scroll");
  	$("body").css("width", "100%");
  });
@@ -137,30 +117,15 @@ $(".gallery-grid a").click(function makeLightBox() {
 
 
 
-// var boxWidth = $(".lightbox-box").children().width(); 
-// var boxHeight = $(".lightbox-box").children().height();
-// var boxAspectRatio = (boxWidth/boxHeight);
 
 
-// function rspImg()	{
+	// When someone click on the image DONE
+	// get the identifier of the image (ex 01, 02) DONE
 
-// 	$(".lightbox-box").css("max-width", 100 * boxAspectRatio + "px");
-// 	// $(".lightbox-box").css("max-width","50px");
-// }
+		// show a new box with the proper image placed DONE
 
-// rspImg();
-
-
-
-
-
-	// When someone click on the image
-	// get the identifier of the image (ex 01, 02)
-
-		// show a new box with the proper image placed
-
-			// obs: this box has to be styles according to the image provided
-			// the rest of the page have to be defoused
+			// obs: this box has to be styles according to the image provided DONE
+			// the rest of the page have to be defoused DONE
 
 		// show previus and next button
 
